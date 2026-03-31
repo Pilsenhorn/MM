602869c86db492d320e00a67ad54dcc5bea8944c
Pilsenhorn <pilsenhorn@gmail.com>
2026-03-31 16:16:03 +0200

    redesign mobile responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    cacheDir: '.astro/vite',
     plugins: [tailwindcss()]
   }
 });
